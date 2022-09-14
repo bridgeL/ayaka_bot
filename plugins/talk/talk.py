19736dd7087ff9f95da238b1dbe067bb7208d9f3
Su <wxlxy316@163.com>
2022-09-14 19:30:29 +0800

    ok

---
+++ plugins/talk/talk.py
@@ -0,0 +1,17 @@
+from ayaka import *
+from .corpus import Corpus
+
+corpus = Corpus()
+
+app = AyakaApp('talk', no_storage=True)
+app.help = "命令式傻瓜聊天机器人"
+
+
+@app.on_text()
+async def talk():
+    msg = str(app.message)
+    ans = corpus.search(msg, True)
+    if ans:
+        await app.send(Message(ans))
+        return True
+    return False
